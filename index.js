dec90806eee5abdab9bd9aa0ddffb7f6cbac3026
Félix Galindo <galindo.fe@gmail.com>
2019-06-13 18:48:56 -0400

    cors

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 //Conectar mongo
 mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost/restapis', {
-    useNewUrlParser: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
 //Crear servidor
@@ -17,6 +18,9 @@ const app = express();
 //Body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Habilitar cors
+app.use(cors());
 
 //Rutas
 app.use('/', routes());
